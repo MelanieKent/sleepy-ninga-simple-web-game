2c67af72002a62498f186eb3299d1084d0e866f4
J0ELh <joelhempel35@gmail.com>
2023-01-21 12:51:47 -0800

    adding canvas to debug

---
+++ gameApp.js
@@ -0,0 +1,19 @@
+const Application = PIXI.Application;
+
+const app = new Application({
+    width: 1000,
+    height: 1000,
+
+});
+
+app.renderer.background.color = 0x00008B;
+
+const Graphics = PIXI.Graphics;
+
+//the whole game area in which the game is played
+const playArea = new Graphics();
+playArea.beginFill(0x00008B);
+playArea.drawRect(0, 0, 1000, 1000);
+playArea.endFill();
+
+app.stage.addChild(playArea);
