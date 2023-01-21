402f9736cbfb4d72d7cf6121441ebd40766db551
J0ELh <joelhempel35@gmail.com>
2023-01-21 14:54:05 -0800

    merged player with line

--- gameApp.js
+++ gameApp.js
@@ -1,9 +1,12 @@
 const Application = PIXI.Application;
 
+//CONSTANTS
 var localServerName = 'http://127.0.0.1:5500';
-
 var gameWidth = 1200;
 var gameHeight = 500;
+var lineY = 365;
+
+
 const app = new Application({
     width: gameWidth,
     height: gameHeight,
@@ -25,11 +28,31 @@ app.stage.addChild(playArea);
 document.body.appendChild(app.view);
 
 
+//player
 const playerTexture = PIXI.Texture.from(localServerName + '//images/player.png');
-
-// const playerTexture = PIXI.Texture.from('*//images/player.png');
 const playerSprite = new PIXI.Sprite(playerTexture);
+playerSprite.scale.set(0.05, 0.05);
+playerSprite.x = 100;
+playerSprite.y = 300
 app.stage.addChild(playerSprite);
+
+
+
+const line =  new Graphics();
+line.lineStyle(20, 0x000000,1)
+.moveTo(0,lineY)
+.lineTo(gameWidth,lineY);
+
+app.stage.addChild(line);
+
+playerSprite.interactive = true;
+
+playerSprite.on('pointerdown', function() {
+    playerSprite.scale.x += 0.1;
+    playerSprite.scale.y += 0.1;
+});
+
+
 
 
 // document.body.appendChild(playerSprite);
