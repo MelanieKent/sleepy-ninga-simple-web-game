2354233b2ae45eeea17db2b76401f1f66eb32777
Triscuit8 <tristontsui@gmail.com>
2023-01-21 15:02:22 -0800

    Added obstacles

--- gameApp.js
+++ gameApp.js
@@ -36,12 +36,38 @@ playerSprite.x = 100;
 playerSprite.y = 300
 app.stage.addChild(playerSprite);
 
+//obstacles
+const obstacle1 = new Graphics();
+obstacle1.beginFill(0xFFFFFF)
+.drawPolygon([
+   450, 450,
+   600, 400,
+   650, 500,
+   400, 500
+])
+.endFill();
+
+const obstacle2 = new Graphics();
+obstacle2.beginFill(0xFFFFFF);
+obstacle2.drawRect(50, 400, 100, 100);
+obstacle2.endFill();
 
 
+const obstacle3 = new Graphics();
+obstacle3.beginFill(0xFFFFFF)
+.drawEllipse(775, 500, 100, 50)
+.endFill();
+
+
+//line
 const line =  new Graphics();
 line.lineStyle(20, 0x000000,1)
 .moveTo(0,lineY)
 .lineTo(gameWidth,lineY);
+
+app.stage.addChild(obstacle1);
+app.stage.addChild(obstacle2);
+app.stage.addChild(obstacle3);
 
 app.stage.addChild(line);
 
@@ -51,6 +77,15 @@ playerSprite.on('pointerdown', function() {
     playerSprite.scale.x += 0.1;
     playerSprite.scale.y += 0.1;
 });
+
+// app.stage.addChild(playArea);
+
+// app.stage.addChild(obstacle1);
+
+// app.stage.addChild(obstacle2);
+
+// app.stage.addChild(obstacle3); 
+
 
 
 
