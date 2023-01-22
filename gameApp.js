3230fb10b689ca0b6c23092ffbd248e2108a9684
Melanie Kent <melkent@students.cs.ubc.ca>
2023-01-21 16:01:30 -0800

    deleted unnecessary comments

--- gameApp.js
+++ gameApp.js
@@ -13,28 +13,7 @@ app.renderer.background.color = 0xffffff;
 
 const Graphics = PIXI.Graphics;
 
-//the whole game area in which the game is played
-// const playArea = new Graphics();
-// playArea.beginFill(0x00008B);
-// playArea.drawRect(gameWidth/2 - 1200/2, gameHeight/2 - 1000/2, 1200, 1000);
-// playArea.endFill();
-// playArea.position(200, 200);
-
-// app.stage.addChild(playArea);
-
 document.body.appendChild(app.view);
-
-
-// const playerTexture = PIXI.Texture.from(localServerName + '//images/back.jpg');
-
-// const playerSprite = new PIXI.Sprite(
-//     playerTexture,
-//     app.screen.width,
-//     app.screen.height);
-
-// app.stage.addChild(playerSprite);
-
-// document.body.appendChild(playerSprite);
 
 const backTexture = PIXI.Texture.from(localServerName + '//images/back.jpg');
 
@@ -51,15 +30,9 @@ app.ticker.add(function() {
     backSprite.tilePosition.x -= 0.5;
 })
 
-
 app.stage.addChild(backSprite);
 
 document.body.appendChild(playerSprite);
 
 
-// const backTexture = PIXI.Texture.from(localServerName + '//images/background.png');
-// const backSprite = new PIXI.TilingSprite(backTexture,
-//     app.screen.height);
-
-// app.stage.addChild(backSprite);
 
