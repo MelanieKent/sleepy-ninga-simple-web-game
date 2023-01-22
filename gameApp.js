ad230ad413d809fc6f060d7f5b588797fef59d19
J0ELh <joelhempel35@gmail.com>
2023-01-21 20:46:07 -0800

    added collision checking

--- gameApp.js
+++ gameApp.js
@@ -2,7 +2,7 @@ const Application = PIXI.Application;
 
 
 //CONSTANTS
-var localServerName = 'http://127.0.0.1:5500';
+var localServerName = 'http://127.0.0.1:5500/';
 var gameWidth = 1200;
 var gameHeight = 645;
 var lineY = 365;
@@ -130,7 +130,64 @@ app.ticker.add(() => {
     if (obstacle2.x < -1200) {
       obstacle2.x = 1000;
     }
+    var isOverlapping = checkOverlap(obstacle2, obstacle2.x, obstacle2.y, playerSprite.x, playerSprite.y, playerSprite.x + playerSprite.width, playerSprite.y + playerSprite.height)
+
+    if (isOverlapping) {
+      app.stage.addChild(obstacle1);
+    }
 });
+
+//###########################################################################################
+// CHECKOVERLAP TAKEN FROM https://www.geeksforgeeks.org/check-if-any-point-overlaps-the-given-circle-and-rectangle/
+//###########################################################################################
+// Javascript implementation to check if any
+// point overlaps the given circle
+// and rectangle
+
+// Function to check if any point
+// overlaps the given circle
+// and rectangle
+function checkOverlap(R, Xc, Yc, X1, Y1, X2, Y2)
+{
+
+	// Find the nearest point on the
+	// rectangle to the center of
+	// the circle
+	let Xn = Math.max(X1, Math.min(Xc, X2));
+	let Yn = Math.max(Y1, Math.min(Yc, Y2));
+	
+	// Find the distance between the
+	// nearest point and the center
+	// of the circle
+	// Distance between 2 points,
+	// (x1, y1) & (x2, y2) in
+	// 2D Euclidean space is
+	// ((x1-x2)**2 + (y1-y2)**2)**0.5
+	let Dx = Xn - Xc;
+	let Dy = Yn - Yc;
+	return (Dx * Dx + Dy * Dy) <= R * R;
+}
+
+
+// Driver Code
+	
+	// let R = 1;
+	// let Xc = 0, Yc = 0;
+	// let X1 = 1, Y1 = -1;
+	// let X2 = 3, Y2 = 1;
+	
+	// if(checkOverlap(R, Xc, Yc,
+	// 				X1, Y1,
+	// 				X2, Y2))
+	// {
+	// 	document.write("True" + "\n");
+	// }
+	// else
+	// {
+	// 	document.write("False");
+	// }
+	
+
 
 app.stage.addChild(obstacle2);
 
