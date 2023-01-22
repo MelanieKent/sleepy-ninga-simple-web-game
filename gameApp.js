25d294c01cc2459dd0a2041ee1fa8839faef71de
Melanie Kent <melkent@students.cs.ubc.ca>
2023-01-21 18:56:10 -0800

    added ninja

--- gameApp.js
+++ gameApp.js
@@ -116,7 +116,7 @@ obstacle3.beginFill(0xFFFFFF)
 
 
 //player
-const playerTexture = PIXI.Texture.from(localServerName + '//images/player.png');
+const playerTexture = PIXI.Texture.from(localServerName + '//images/ninja.png');
 const playerSprite = new PIXI.Sprite(playerTexture);
 playerSprite.scale.set(1, 1);
 playerSprite.x = 100;
